47af854eaa21e42466c99965bf0396ef45b9cace
zadkielb <156140230+zadkielb@users.noreply.github.com>
2024-02-01 16:40:21 -0600

    Add files via upload

---
+++ index.js
@@ -0,0 +1,12 @@
+let saludo = "¡Hello World! I'm Zadkiel";
+let hola = "Hello World";
+const Name = "Zadkiel";
+let array = [1, 2, 3, 4, 5];
+let clima = "La lluvia en Sevilla es una maravilla";
+let lluvia = "lluvia";
+
+console.log(saludo, "\n", "Mi nombre en mayúsculas:", Name.toUpperCase(), "\n", "Mi nombre en minúsculas:", Name.toLowerCase());
+console.log("El mensaje 'Hello World' tiene", hola.length, "caracteres.");
+console.log("El tercer número del array es", array[2], "\n",
+    "Se le agrega el número que sigue en orden al final, resultando en el array:", array);
+console.log("Se encontró la palabra 'lluvia' en el texto", clima, "por lo que el resultado es:", clima.includes(lluvia));
